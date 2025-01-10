fe365a93ad3694bcff009a976fddeb65d9a31f14
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-01-10 10:23:40 -0800

    added service component

---
+++ src/service.js
@@ -0,0 +1,66 @@
+
+const ServiceComponent = () => {
+    return ( 
+        <div className="service">
+                    <h3>Plans</h3>
+            <p>Our Services</p>
+            <p>Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit.</p>
+            <div className="cards">
+                <div className="card">
+                    <p>Basic</p>
+                    <p>$100
+                    /month</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam</p>
+                    <div className="list">
+                        <ul>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                        </ul>
+                    </div>
+                    <button className='more'>Learn More</button>
+                </div>
+                <div className="card">
+                    <p>Plus</p>
+                    <p>$250
+                    /month</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam</p>
+                    <div className="list">
+                        <ul>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                        </ul>
+                    </div>
+                    <button className='more'>Learn More</button>
+                </div>
+                <div className="card">
+                    <p>Pro</p>
+                    <p>$400
+                    /month</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam</p>
+                    <div className="list">
+                        <ul>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                            <li><img src="" className="mark"/>Lorem ipsum, dolor sit </li>
+                        </ul>
+                    </div>
+                    <button className='more'>Learn More</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ServiceComponent;
+
+
+
