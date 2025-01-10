105b6d8de2b027caf754483c02c1daa9983934fa
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-01-10 02:42:53 -0800

    added header to contact page

---
+++ src/contact.js
@@ -0,0 +1,14 @@
+const contactComponent = () => {
+    return ( 
+        <div className="contact">
+            <h3>Contact Us</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit.</p>
+            <div className="form">
+
+            </div>
+        </div>
+    );
+}
+
+export default contactComponent;
